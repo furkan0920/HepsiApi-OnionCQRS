c2b7733d823332cd1fa8d88ffda74f1d8c23e1c4
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2024-04-27 18:32:49 +0300

    Temel Entityler Oluşturuldu

---
+++ Core/Hepsi.Domain/Entities/Detail.cs
@@ -0,0 +1,29 @@
+﻿using Hepsi.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hepsi.Domain.Entities
+{
+    public class Detail:EntityBase
+    {
+        public Detail()
+        {
+            
+        }
+
+        public Detail(string title, string description, int categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
