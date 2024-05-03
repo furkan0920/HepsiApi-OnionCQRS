22c9fdd120b4e51d7cb4b3efad9e7cf08e6aa6e9
Furkan Ayakdaş <72611180+furkan0920@users.noreply.github.com>
2024-05-03 08:25:56 +0300

    redis cache yapısı oluşturuldu.

---
+++ Core/Hepsi.Application/Beheviors/RedisCacheBehevior.cs
@@ -0,0 +1,39 @@
+﻿using Hepsi.Application.Interface.RedisCache;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hepsi.Application.Beheviors
+{
+    public class RedisCacheBehevior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IRedisCacheService redisCacheService;
+
+        public RedisCacheBehevior(IRedisCacheService redisCacheService)
+        {
+            this.redisCacheService = redisCacheService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if(redisCacheService is  ICacheableQuery query)
+            {
+                var cacheKey=query.CacheKey;
+                var cacheTime=query.CacheTime;
+                var cacheData = await redisCacheService.GetAsync<TResponse>(cacheKey);
+                if (cacheData is not null)
+                    return cacheData;
+                var response = await next();
+                if(response is not null)
+                    await redisCacheService.SetAsync(cacheKey, response,DateTime.Now.AddMinutes(cacheTime));
+
+                return response;
+
+            }
+            return await next();    
+        }
+    }
+}
